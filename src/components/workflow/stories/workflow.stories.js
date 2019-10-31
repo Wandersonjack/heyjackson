2b80f54da739693ee4a54186be3c435003552cce
Wandersonjack <wander8jackson@gmail.com>
2019-10-31 16:09:29 +0200

    work section

---
+++ src/components/workflow/stories/workflow.stories.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import WorkFlow from "../workflow"
+
+
+const stories = storiesOf("V1|Components/Workflow", module)
+
+stories.add("WorkFlow", () =>   <WorkFlow />)
