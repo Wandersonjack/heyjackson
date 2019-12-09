e7c9c3f2002d873a11feedd773ebb38227b3b408
wandersonjack <wander8jackson@gmail.com>
2019-12-10 01:27:12 +0200

    about me section fix

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -7,25 +7,36 @@ import "../about/styles/about.css"
 export default class About extends Component {
   render() {
     return (
-      <div className="cardbase-about gap-top" id="aboutme-section">
-        <div className={this.props.imgncolor}>
-          <img
-            src={
-              "https://res.cloudinary.com/wander8jackson/image/upload/c_scale,w_789/v1572890094/heyjackson_r1vo4c.png"
-            }
-            className="me-img"
-          />
-        </div>
-        <div className="about_content">
-          <div className="content_case">
-            <Heading2 children="display2" text="Wanderson Jackson" />
-            <Heading4 children="heading4" text="I'm Wanderson Jackson, Product Designer with strong focus on user-centered design, to validate and build digital products that users want, with teams of engineers, executives, and marketing teams, to ensure user needs are aligned with business goals." />
-            <Heading4 children="heading4" text="On free time I enjoy dancing, diving and hiking.
-            Also love to inspire people creating content for my Youtube channel."/>
-            <div>
-              <a href="https://drive.google.com/file/d/1fh9lB-uy08ApMePeDSc5Bq08teY5Ojxm/view?usp=sharing" target="_blank">
-                <Button text="Download cv" children="btn-base color-green" />
-              </a>
+      <div className="container">
+        <div className="cardbase-about gap-top" id="aboutme-section">
+          <div className={this.props.imgncolor}>
+            <img
+              src={
+                "https://res.cloudinary.com/wander8jackson/image/upload/c_scale,w_789/v1572890094/heyjackson_r1vo4c.png"
+              }
+              className="me-img"
+            />
+          </div>
+          <div className="about_content">
+            <div className="content_case">
+              <Heading2 children="display2" text="Wanderson Jackson" />
+              <Heading4
+                children="heading4"
+                text="I'm Wanderson Jackson, Product Designer with strong focus on user-centered design, to validate and build digital products that users want, with teams of engineers, executives, and marketing teams, to ensure user needs are aligned with business goals."
+              />
+              <Heading4
+                children="heading4"
+                text="On free time I enjoy dancing, diving and hiking.
+            Also love to inspire people creating content for my Youtube channel."
+              />
+              <div>
+                <a
+                  href="https://drive.google.com/file/d/1fh9lB-uy08ApMePeDSc5Bq08teY5Ojxm/view?usp=sharing"
+                  target="_blank"
+                >
+                  <Button text="Download cv" children="btn-base color-green" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
