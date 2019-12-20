63f1fad6b5d9477dfc1442eef0353451925297eb
wandersonjack <wander8jackson@gmail.com>
2019-12-20 06:05:15 +0200

    text update

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,21 +10,17 @@ export default class About extends Component {
       <div className="container">
         <Display2 text="About me" children="display2" />
         <div className="cardbase-about gap-top" id="aboutme-section">
-          <div  className="me-img">
-  
-          </div>
+          <div className="me-img"></div>
           <div className="about_content">
             <div className="content_case">
               <Heading2 children="display2" text="Wanderson Jackson" />
               <Heading4
                 children="heading4"
-                text="I'm Wanderson Jackson, Product Designer with a strong focus on user-centered design, to validate and build digital products that users want, with teams of engineers, executives, and marketing teams, to ensure user needs are aligned with business goals.
-                On free time I enjoy dancing, diving, and hiking. Also, love to inspire people creating content for my Youtube channel."
+                text="I'm Wanderson Jackson, Product Designer with a strong focus on user-centered design, to validate and build digital products that users want, with teams of engineers, executives, and marketing teams, to ensure user needs are aligned with business goals."
               />
               <Heading4
                 children="heading4"
-                text="On free time I enjoy dancing, diving and hiking.
-            Also love to inspire people creating content for my Youtube channel."
+                text="On free time I enjoy dancing, diving, and hiking. Also, love to inspire people creating content for my Youtube channel."
               />
               <div>
                 <a
